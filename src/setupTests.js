453a93944c5cb15876ac46a66063d75bc4e519d4
Anand <anandmondem7854@gamail.com>
2024-08-26 14:18:53 +0000

    solution

---
+++ src/setupTests.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom'
+import {configure as eConfigure} from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+import {configure} from '@testing-library/react'
+
+configure({testIdAttribute: 'data-testid'})
+
+eConfigure({adapter: new Adapter()})
